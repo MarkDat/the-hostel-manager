da1156503e824b5ca39cae5ccccc69390d994246
Dat Luong <dat.luong@enlabsoftware.com>
2022-11-04 14:49:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { HashRouter as Router } from 'react-router-dom';
+
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import LoadPanel from 'devextreme-react/load-panel';
+
+import logo from './logo.svg';
+import './App.css';
+
+import Content from './Content';
+import { AuthProvider, useAuth } from './contexts/auth';
+import { useScreenSizeClass } from './utils/media-query';
+import { NavigationProvider } from './contexts/navigation';
+import UnauthenticatedContent from './UnauthenticatedContent';
+
+
+const App = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <LoadPanel visible={true} />;
+  }
+
+  if (user) {
+    return <Content />;
+  }
+
+  return <UnauthenticatedContent />;
+}
+
+export default function Root() {
+  const screenSizeClass = useScreenSizeClass();
+
+  return (
+    <Router>
+      <AuthProvider>
+        <NavigationProvider>
+          <div className={`app ${screenSizeClass}`}>
+            <App />
+          </div>
+        </NavigationProvider>
+      </AuthProvider>
+    </Router>
+  );
+}
---
+++ src/app-info.js
@@ -0,0 +1,5 @@
+const appInfo = {
+    title: 'The Inn Manager'
+};
+export default appInfo;
+
