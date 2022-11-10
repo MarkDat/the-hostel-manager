75ca48984671b518db485f424a0375013d8da5a6
MarkDat <mark.dat.code@gmail.com>
2022-11-10 09:55:35 +0700

    Fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 export { default as dashboardRoute } from './dashboard/dashboard.route';
-export { default as roomRoute } from './room/room.route';
+
 export { default as roomListRoute } from './room/list/room-list.route';
+export { default as roomRoute } from './room/room.route';
+
 export { default as settingRoute } from './setting/setting.route';
