64eb2da5ced1cc349edf72b5adf9a77119553977
MarkDat <mark.dat.code@gmail.com>
2022-11-14 17:04:21 +0700

    Add

--- src/components/room-list/RoomList.js
+++ src/components/room-list/RoomList.js
@@ -1,35 +1,61 @@
-import { ActionCellTemplate } from 'components';
-import { DataGrid, Lookup } from 'devextreme-react';
-import { Column, FilterRow, Pager, Paging } from 'devextreme-react/data-grid';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import { ActionCellTemplate } from "components";
+import { DataGrid, Lookup } from "devextreme-react";
+import { Column, FilterRow, Pager, Paging } from "devextreme-react/data-grid";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 export default function RoomList() {
   const [dataSource, setDataSource] = useState([]);
 
   useEffect(() => {
     setDataSource([
-      { id: 1, roomCode: 'PT1', roomName: 'Vợ Chồng Hoà', electricityNumber: 4952, waterNumber: 2651 },
-      { id: 2, roomCode: 'PT2', roomName: 'Vợ Chồng Thuỷ', electricityNumber: 4952, waterNumber: 2651 },
-      { id: 3, roomCode: 'PT3', roomName: 'Bon - Sinh viên', electricityNumber: 4952, waterNumber: 2651 },
-      { id: 4, roomCode: 'PT4', roomName: 'Thằng ku cháu', electricityNumber: 4952, waterNumber: 2651 }
+      {
+        id: 1,
+        roomCode: "PT1",
+        roomName: "Vợ Chồng Hoà",
+        electricityNumber: 4952,
+        waterNumber: 2651,
+      },
+      {
+        id: 2,
+        roomCode: "PT2",
+        roomName: "Vợ Chồng Thuỷ",
+        electricityNumber: 4952,
+        waterNumber: 2651,
+      },
+      {
+        id: 3,
+        roomCode: "PT3",
+        roomName: "Bon - Sinh viên",
+        electricityNumber: 4952,
+        waterNumber: 2651,
+      },
+      {
+        id: 4,
+        roomCode: "PT4",
+        roomName: "Thằng ku cháu",
+        electricityNumber: 4952,
+        waterNumber: 2651,
+      },
     ]);
   }, []);
 
-  const renderGridActionCell = () => {
-    return (
-      <> {<ActionCellTemplate />} </>
-    );
+  const onEditClicked = (event, data) => {
+    console.log('edit clicked: ',data);
+  }
+
+  const onDeleteClicked = (event, data) => {
+    console.log('delete clicked: ',data);
   }
 
   return (
     <>
-      <h2 className={'content-block'}>Room List</h2>
-      <div className={'content-block'}>
+      <h2 className={"content-block"}>Room List</h2>
+      <div className={"content-block"}>
         <DataGrid
-          className={'dx-card wide-card'}
+          className={"dx-card wide-card"}
           dataSource={dataSource}
-          keyExpr='id'
+          keyExpr="id"
           showBorders={false}
           showRowLines={false}
           focusedRowEnabled={true}
@@ -40,24 +66,26 @@ export default function RoomList() {
           <Pager showPageSizeSelector={true} showInfo={true} />
           <FilterRow visible={true} />
 
-          <Column dataField={'roomCode'} caption={'Mã Phòng'} />
+          <Column dataField={"roomCode"} caption={"Mã Phòng"} />
+          <Column dataField={"roomName"} caption={"Tên Phòng"} />
           <Column
-            dataField={'roomName'}
-            caption={'Tên Phòng'}
-          />
-          <Column
-            dataField={'electricityNumber'}
+            dataField={"electricityNumber"}
             alignment="left"
-            caption={'Số Điện'}
+            caption={"Số Điện"}
           />
           <Column
-            dataField={'waterNumber'}
+            dataField={"waterNumber"}
             alignment="left"
-            caption={'Số Nước'}
+            caption={"Số Nước"}
           />
-          <Column caption={'Hành động'} cellRender={renderGridActionCell} />
+          <Column
+            width={60}
+            fixed={true}
+            fixedPosition="right"
+            cellRender={(e) => <ActionCellTemplate event={e} onEditClicked={onEditClicked} onDeleteClicked={onDeleteClicked} />}
+          />
         </DataGrid>
       </div>
     </>
-  )
+  );
 }
--- src/components/shared/ActionCellTemplate/ActionCellTemplate.js
+++ src/components/shared/ActionCellTemplate/ActionCellTemplate.js
@@ -1,8 +1,21 @@
-export default function ActionCellTemplate() {
+import './ActionCellTemplate.scss'
+
+export default function ActionCellTemplate({event, onEditClicked, onDeleteClicked}) {
+
+    const handleEditClicked = (e) => {
+        onEditClicked(e, event.data);
+    }
+
+    const handleDeleteClicked = (e) => {
+        onDeleteClicked(e, event.data);
+    }
+    
     return (
         <>
-          <i class="fa-regular fa-pen-to-square"></i>
-          <i class="fa-regular fa-trash"></i>
+            <div className="action-cell">
+                <i onClick={handleEditClicked} className="action-cell--edit fa-regular fa-pen-to-square"></i>
+                <i onClick={handleDeleteClicked} className="action-cell--delete fa-regular fa-trash"></i>
+            </div>
         </>
       );
 }
