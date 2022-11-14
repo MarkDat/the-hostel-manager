b8768ddf864d020b306b1e6bb2713ef2de9db09f
MarkDat <mark.dat.code@gmail.com>
2022-11-14 10:56:57 +0700

    Devided

--- src/Content.js
+++ src/Content.js
@@ -1,19 +1,5 @@
-import { Routes, Route, Navigate, Outlet } from "react-router-dom";
-import appInfo from "./app-info";
-import routes from "./app-routes";
-import { SideNavOuterToolbar as SideNavBarLayout } from "./layouts";
-import { Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
 export default function Content() {
-  return (
-    <SideNavBarLayout title={appInfo.title}>
-      <Outlet/>
-      <Footer>
-        Copyright © 2011-{new Date().getFullYear()} {appInfo.title} Inc.
-        <br />
-        All trademarks or registered trademarks are property of their respective
-        owners.
-      </Footer>
-    </SideNavBarLayout>
-  );
+  return (  <Outlet/> );
 }
--- src/pages/landlord/landlord.js
+++ src/pages/landlord/landlord.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Footer } from "components";
+import appInfo from "app-info";
+import { SideNavOuterToolbar as SideNavBarLayout } from "layouts";
 
 export default function LandLord() {
-  return <Outlet />;
+  return (
+    <SideNavBarLayout title={appInfo.title}>
+      <Outlet/>
+      <Footer>
+        Copyright © 2011-{new Date().getFullYear()} {appInfo.title} Inc.
+        <br />
+        All trademarks or registered trademarks are property of their respective
+        owners.
+      </Footer>
+    </SideNavBarLayout>
+  );
 }
